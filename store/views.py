ec293e37b14084fc199077e8768f7b6e3435453f
monsur22 <monsurahmedshafiq@gmail.com>
2022-04-03 15:21:51 +0600

    Product details complete

--- store/views.py
+++ store/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import get_object_or_404, render
 from store.models import Product
 from category.models import Category
 
-def store(request, slug = None):
+def store(request, category_slug = None):
     categories = None
     products = None
-    if slug != None:
-        categories = get_object_or_404(Category, slug=slug)
+    if category_slug != None:
+        categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category = categories, is_available = True)
         productCount = products.count()
 
@@ -19,3 +19,13 @@ def store(request, slug = None):
         'productCount':productCount,
     }
     return render(request, 'store/store.html',contex)
+
+def product_details(request, category_slug, product_slug):
+    try:
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+    context = {
+        'single_product':single_product,
+    }
+    return render(request, 'store/product_detail.html', context)
